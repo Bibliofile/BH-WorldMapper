8872e74134c820587607a3d12505ee4b2f69406b
Bibliofile <gtbirk@gmail.com>
2015-06-21 16:00:32 -0600

    Initial commit
    The current version of source code

---
+++ WorldMapGeneratorCSharp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WorldMapGeneratorCSharp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btnFolderPick = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.btnGenerate = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnFolderPick
+			// 
+			this.btnFolderPick.Location = new System.Drawing.Point(13, 13);
+			this.btnFolderPick.Name = "btnFolderPick";
+			this.btnFolderPick.Size = new System.Drawing.Size(99, 23);
+			this.btnFolderPick.TabIndex = 0;
+			this.btnFolderPick.Text = "Choose Folder";
+			this.btnFolderPick.UseVisualStyleBackColor = true;
+			this.btnFolderPick.Click += new System.EventHandler(this.btnFolderPick_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.radioButton3);
+			this.groupBox1.Controls.Add(this.radioButton2);
+			this.groupBox1.Controls.Add(this.radioButton1);
+			this.groupBox1.Location = new System.Drawing.Point(13, 43);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(99, 93);
+			this.groupBox1.TabIndex = 1;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "World Size";
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(7, 68);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(42, 17);
+			this.radioButton3.TabIndex = 2;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "16x";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(7, 44);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(36, 17);
+			this.radioButton2.TabIndex = 1;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "4x";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(7, 20);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(58, 17);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Normal";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+			// 
+			// btnGenerate
+			// 
+			this.btnGenerate.Location = new System.Drawing.Point(13, 143);
+			this.btnGenerate.Name = "btnGenerate";
+			this.btnGenerate.Size = new System.Drawing.Size(99, 23);
+			this.btnGenerate.TabIndex = 2;
+			this.btnGenerate.Text = "Generate Map";
+			this.btnGenerate.UseVisualStyleBackColor = true;
+			this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(13, 173);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(100, 10);
+			this.progressBar1.TabIndex = 3;
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(12, 189);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(100, 23);
+			this.btnSave.TabIndex = 4;
+			this.btnSave.Text = "Save Map";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// folderBrowserDialog1
+			// 
+			this.folderBrowserDialog1.ShowNewFolderButton = false;
+			// 
+			// saveFileDialog1
+			// 
+			this.saveFileDialog1.FileName = "map.bmp";
+			this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(124, 223);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.btnGenerate);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.btnFolderPick);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnFolderPick;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Button btnGenerate;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+	}
+}
+
---
+++ WorldMapGeneratorCSharp/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WorldMapGeneratorCSharp
+{
+	public partial class Form1 : Form
+	{
+		int worldNormal = 16384;
+		int world4x = 65536;
+		int world16x = 262144;
+		Bitmap picture;
+		Boolean canGenerate;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			btnGenerate.Enabled = false;
+			btnSave.Enabled = false;
+			canGenerate = false;
+		}
+
+		private void btnFolderPick_Click(object sender, EventArgs e)
+		{
+			DialogResult result;
+			progressBar1.Value = 0;
+			result = folderBrowserDialog1.ShowDialog();
+			if (canGenerate && result == DialogResult.OK) 
+			{
+				canGenerate = false;
+				btnGenerate.Enabled = true;
+			} 
+			else if (result == DialogResult.OK)
+			{
+				canGenerate = true;
+			}
+		}
+
+		//Radio Buttons
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		{
+			if (canGenerate)
+			{
+				btnGenerate.Enabled = true;
+			}
+			else if (radioButton1.Checked)
+			{
+				canGenerate = true;
+			}
+		}
+		private void radioButton2_CheckedChanged(object sender, EventArgs e)
+		{
+			if (canGenerate)
+			{
+				btnGenerate.Enabled = true;
+			}
+			else if (radioButton2.Checked)
+			{
+				canGenerate = true;
+			}
+		}
+		private void radioButton3_CheckedChanged(object sender, EventArgs e)
+		{
+			if (canGenerate)
+			{
+				btnGenerate.Enabled = true;
+			}
+			else if (radioButton3.Checked)
+			{
+				canGenerate = true;
+			}
+		}
+
+		//Here is where all generation is done
+		private void btnGenerate_Click(object sender, EventArgs e)
+		{
+			string location = folderBrowserDialog1.SelectedPath;
+			int size;
+			RadioButton modeRadio;
+
+			//Get the selected world size
+			modeRadio = groupBox1.Controls.OfType<RadioButton>().FirstOrDefault(n => n.Checked);
+			if (modeRadio.Name == "radioButton1")
+			{
+				size = worldNormal;
+			}
+			else if (modeRadio.Name == "radioButton2")
+			{
+				size = world4x;
+			}
+			else if (modeRadio.Name == "radioButton3")
+			{
+				size = world16x;
+			}
+			else
+			{
+				MessageBox.Show("Size error, please select an allowed size");
+				return;
+			}
+
+			string[] fileArray = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+			Bitmap result = new Bitmap(size, 1024, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+			progressBar1.Maximum = fileArray.Length;
+
+			using (Graphics g = Graphics.FromImage(result))
+			{
+				foreach (string picLoc in fileArray)
+				{
+					string[] seperators = { "\\", "_", "." };
+					string[] picSplit = picLoc.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+					string xCoord = picSplit[picSplit.Length - 3];
+					string yCoord = picSplit[picSplit.Length - 2];
+
+					using (Bitmap layer = new Bitmap(Image.FromFile(picLoc)))
+					{
+						g.DrawImage(layer, (int.Parse(xCoord) * 32), (1024 - int.Parse(yCoord) * 32), 32, 32);
+					}
+					progressBar1.Increment(1);
+				}
+			}
+			picture = result;
+			btnSave.Enabled = true;
+		}
+
+		//Save the image as BMP
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			saveFileDialog1.ShowDialog();
+		}
+		private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			picture.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+		}
+
+	}
+}
